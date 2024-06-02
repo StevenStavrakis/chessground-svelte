5d48e5e422c88767882fe0c50e8e3d1fbc8f5d7a
Steven Stavrakis <s.a.stavrakis@gmail.com>
2024-06-02 11:41:58 -0400

    change import

--- src/lib/Chessground.svelte
+++ src/lib/Chessground.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { Config } from 'chessground/config';
 	import type { FEN, Color, Key } from 'chessground/types';
-	import { ChessgroundViewmodel } from './ChessgroundViewmodel.svelte.js';
+	import { ChessgroundViewmodel } from './ChessgroundViewmodel.svelte.ts';
 
 	let {
 		vm,
